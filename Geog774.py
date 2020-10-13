58f64a3883e7907acf8941952d6d42d4cbb15707
EinsteinFrizz <52115999+EinsteinFrizz@users.noreply.github.com>
2020-10-14 11:39:13 +1300

    actual ponsonby this time

--- Geog774.py
+++ Geog774.py
@@ -25,7 +25,7 @@ import json #was getting error 'name json is not defined' for dump
 endpoint = 'https://api.yelp.com/v3/businesses/search'
 headers = {'Authorization':'Bearer l_zg3FKea0qKiF8ql_cEcSzTJe_keKB2S_O15T2ZV-BitVeLQHnfjkC3Z7Ukkk2n9h61KDb04iw3CDPhjLIxdALd85B4Fp9f5_nA47Br5oDmlC9Zjd-yo1Kg6Ql0X3Yx'} #this is my API key
 
-params = {'latitude':'-36.8411405' , 'longitude':'174.7515079' , 'limit':'50' , 'radius':'500'}
+params = {'latitude':'-36.848459' , 'longitude':'174.743834' , 'limit':'50' , 'radius':'500'}
 
 payload = requests.get(endpoint,params=params,headers=headers).json()
 
