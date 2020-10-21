bb5f64b0ecc4cc6b9c82e56c300d6f1b32e032f8
EinsteinFrizz <52115999+EinsteinFrizz@users.noreply.github.com>
2020-10-21 14:13:21 +1300

    wtf happened to the .py, did ponsonby

--- Geog774.py
+++ Geog774.py
@@ -190,7 +190,6 @@ logging.info("starting LDA model")
 
 model = models. ldamodel.LdaModel(corpus_tfidf, id2word=dictionary, alpha=0.001, num_topics=20, update_every=0, passes=50)
 
-<<<<<<< Updated upstream
 pp(model.show_topics())
 
 # %% OUTPUT TO CSV
@@ -379,7 +378,5 @@ csv_file.close()
 ##    #current error is that the stuff inside writerows() needs to be iterable
 ##
 ##csv_file.close()
-=======
+#
 #pp(model.show_topics()) #prints out the topics and their match value
-
->>>>>>> Stashed changes
